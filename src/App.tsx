256ecbd1cc82c4c29c87054a0511058e20742149
pranav5002 <pranavsinghal2020@gmail.com>
2026-04-11 01:22:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+
+const App = () => {
+  return <Router>
+    <div className='flex h-screen'>
+      <Sidebar/>
+    </div>
+  </Router>
+  
+}
+
+export default App
